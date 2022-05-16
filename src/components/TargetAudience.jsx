1e8dca08293772b56efda4dfaece58c04787467e
Magus1403 <aguspumarejo@gmail.com>
2022-05-16 18:49:27 -0500

    Tried to add routes

--- src/components/TargetAudience.jsx
+++ src/components/TargetAudience.jsx
@@ -38,7 +38,7 @@ function TargetAudience() {
       <div className="chart">
         <ResponsiveContainer width="100%" height="100%">
           <BarChart width={500} height={300} data={data}>
-            <Tooltip cursor={{ fill: "#3d741d7a" }} />
+            <Tooltip cursor={{ fill: "#475463" }} />
             <defs>
               <linearGradient id="colorview" x1="0" y1="0" x2="0" y2="1">
                 <stop
