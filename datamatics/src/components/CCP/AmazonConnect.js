41d8aa8ef64eac9e999655a25d3df0f6b0e5ac0d
SkohnE <s.kohne99@gmail.com>
2022-05-24 10:19:36 -0500

    add main functions to amazonConnect.js

--- datamatics/src/components/CCP/AmazonConnect.js
+++ datamatics/src/components/CCP/AmazonConnect.js
@@ -3,11 +3,17 @@ import CCP from "./CCP";
 
 import classes from "./AmazonConnect.module.css";
 
-const AmazonConnect = () => {
+const AmazonConnect = (props) => {  
+  const { userActive, loging,setUserActive } = props;
   return (
     <div id={"ccp"} className={classes.ccpPosition}>
       {isBrowserCompatible() && (
-        <CCP instanceURL={"https://ac-datamatics.my.connect.aws/ccp-v2"} />
+        <CCP
+          userActive={userActive}
+          loging={loging}
+          setUserActive={setUserActive}
+          instanceURL={"https://ac-datamatics.my.connect.aws/ccp-v2"}
+        />
       )}
     </div>
   );
