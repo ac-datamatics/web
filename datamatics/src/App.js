4586944a1b23139005b06807ba66409d5945259d
SkohnE <s.kohne99@gmail.com>
2022-05-24 10:25:34 -0500

    hidden use userActive and send function to Connect

--- datamatics/src/App.js
+++ datamatics/src/App.js
@@ -6,11 +6,20 @@ import Training from "./components/Tabs/Training/Training";
 import ScreenRecording from "./components/Tabs/ScreenRecording";
 import AmazonConnect from "./components/CCP/AmazonConnect";
 import "./App.css";
+import React from "react";
 
-function App() {
+function App(props) {
+  const { userActive, loging, setUserActive } = props.AuthFunction();
+
+  if (!userActive) {
+    loging();
+  }
+
   return (
     <div className="root">
-      <div className="sideBarContainer">
+      <div hidden={userActive}>Loading....</div>
+
+      <div className="sideBarContainer" hidden={!userActive}>
         <Sidebar className="sideBar">
           <Route path="*" exact>
             <Redirect to="/home" />
@@ -29,8 +38,12 @@ function App() {
           </Route> */}
         </Sidebar>
       </div>
-      <div className="amazonConnectContainer">
-        <AmazonConnect />
+      <div className="amazonConnectContainer" hidden={!userActive}>
+        <AmazonConnect
+          setUserActive={setUserActive}
+          userActive={userActive}
+          loging={loging}
+        />
       </div>
     </div>
   );
--- datamatics/src/index.js
+++ datamatics/src/index.js
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import AuthFunction from "./functions/mainFunction.jsx";
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App AuthFunction={AuthFunction} />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
