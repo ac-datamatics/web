bbe6001e2eccc6ec2646c7e0efaa29e6fa4f9c40
SkohnE <s.kohne99@gmail.com>
2022-05-26 01:16:59 -0500

    Clean Login code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,11 +10,6 @@ const jump = keyframes`
     transform: translateY(-3px)
   }
 `;
-const Login = styled.h1`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-`;
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -36,34 +31,6 @@ const Wrapper = styled.section`
   align-items: center;
   height: 100%;
   width: 100%;
-`;
-
-const Form = styled.form`
-  margin: 0 auto;
-  width: 100%;
-  max-width: 414px;
-  padding: 1.3rem;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-`;
-
-const Input = styled.input`
-  max-width: 100%;
-  padding: 11px 13px;
-  background: #f9f9fa;
-  color: #f03d4e;
-  margin-bottom: 0.9rem;
-  border-radius: 4px;
-  outline: 0;
-  border: 1px solid rgba(245, 245, 245, 0.7);
-  font-size: 14px;
-  transition: all 0.3s ease-out;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.1), 0 1px 1px rgba(0, 0, 0, 0.1);
-  :focus,
-  :hover {
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
-  }
 `;
 
 const Button = styled.button`
@@ -93,33 +60,16 @@ const Title = styled.h2`
   text-align: center;
 `;
 
-function App() {
-  const [dados, setDados] = useState({
-    email: "",
-    password: ""
-  });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(dados);
-  };
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    setDados(Object.assign(dados, { [name]: value }));
-  };
-
+function LogIn() {
   return (
     <>
       <GlobalStyle />
       <Wrapper>
-        <Form onSubmit={handleSubmit}>
-          <Title>Login to Amazon Connect</Title>
-          <Button>Login</Button>
-        </Form>
+        <Title>Login to Amazon Connect</Title>
+        <Button>Login</Button>
       </Wrapper>
     </>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default LogIn;
