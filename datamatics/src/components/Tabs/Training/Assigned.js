4a2278ce74b0d62a41dc8e86898071c51c60f7a4
GianlucaBeltran <gianlucabeltran@gmail.com>
2022-06-02 16:29:10 -0500

    Added fetch request for assigned videos

--- datamatics/src/components/Tabs/Training/Assigned.js
+++ datamatics/src/components/Tabs/Training/Assigned.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ThumbCard from "./ThumbCard";
 
 import classes from "./Training.module.css";
@@ -33,6 +34,16 @@ const Assigned = () => {
       assignedDate: "16/05/2022",
     },
   ];
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch(
+      "https://2uxbgsvox5.execute-api.us-east-1.amazonaws.com/Datamatics/video?Contact_ID=[Contact_ID]"
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, [])
 
   return (
     <div className={classes.thumbnailCard}>
