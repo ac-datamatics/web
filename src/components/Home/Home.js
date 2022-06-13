29f547257bfb5e13d22274f36c3cc69c9ab464f0
Gianluca Beltran <gianlucabeltran@gmail.com>
2022-06-13 17:54:01 -0500

    Deleted unused imports

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,6 @@ import Streams from "../Streams";
 import Donut from "../Donut";
 import "./Home.css";
 import Welcome from "./Welcome"
-import Assigned from "../Training/Assigned";
-import classes from "../Training/Training.module.css";
-import TrainingTabs from "../Training/TrainingTabs";
 
 export function Home({ username }) {
   return (
@@ -24,14 +21,14 @@ export function Home({ username }) {
 
 
 
-export function HomeSUPERV({username}) {
+export function HomeSUPERV({ username }) {
   return (
     <div className="Home">
       <div style={{
         display: "flex",
         flexDirection: "row",
       }}>
-        <Welcome username={username}/>
+        <Welcome username={username} />
         <Donut />
       </div>
       <Streams />
