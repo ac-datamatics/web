e56977e58aab7da9a47279fe8ceffbcccf0df834
fernandanavamoya <fernava023@gmail.com>
2022-06-13 17:57:22 -0500

    assigned videos and search datepicker display update

--- src/components/Training/ThumbCard.js
+++ src/components/Training/ThumbCard.js
@@ -3,7 +3,6 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import { BsThreeDots } from "react-icons/bs";
 
 import classes from "./ThumbCard.module.css";
 import VideoDetails from "./VideoDetails";
@@ -15,17 +14,10 @@ const ThumbCard = ({ video }) => {
   const handleModalOpen = () => {
     setOpen(true);
   };
-  
 
   return (
     <>
       <Card className={classes.root} raised={true}>
-        <div style={{backgroundColor: '#232f3e', display: 'flex', justifyContent: 'right', padding: '10px 10px 0px 0px'}}>
-          <select className={classes.customSelect}>
-            <option selected="selected">Not Assigned</option>
-            <option>Assigned</option>
-          </select>
-        </div>
         <CardActionArea onClick={handleModalOpen}>
           {/* <CardMedia
             component="img"
--- src/components/Training/Training.js
+++ src/components/Training/Training.js
@@ -2,6 +2,7 @@ import ReactPlayer from "react-player";
 import Assigned from "./Assigned";
 import {useEffect} from "react";
 import ThumbCard from "./ThumbCard";
+import ThumbCardSuperv from './ThumbCardSUPERV';
 import { FiSearch } from "react-icons/fi";
 
 import classes from "./Training.module.css";
@@ -11,13 +12,36 @@ import DatePicker from "react-datepicker";
 import React, { useState } from 'react';
 
 export const Training = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [query, setQuery] = useState("");
+  const [videoInfo, setVideoInfo] = useState([]);
+
   return (
     <div className={classes.newWrap}>
-    <TrainingTabs labels={["Assigned", "General"]}>
-      <div className={classes.contentWrap}>
-        <Assigned />
+      <div className={classes.search}>
+        < FiSearch color="white" size="20px"/>
+        <input className={classes.searchBar} placeholder="Search..." onChange={event => setQuery(event.target.value)} />
       </div>
-    </TrainingTabs>
+      <TrainingTabs labels={["Assigned", "General"]}>
+        <div className={classes.contentWrap}>
+          {videoInfo.filter(video => {
+            if(query === '') {
+              return video;
+            // } else if (video.videoTopic.toLowerCase().includes(query.toLowerCase())) {
+            //   return video;
+            // } else if (video.assignedDate.toLowerCase().includes(query.toLowerCase())) {
+            // return video;
+            } else if (video.agentUsername.toLowerCase().includes(query.toLowerCase()) && 
+            (Date(video.uploadDate).getTime() >= startDate.getTime()) && 
+            (Date(video.uploadDate).getTime() <= endDate.getTime())) {
+              return video;
+            }}).map((video, key) => {
+              return <ThumbCardSuperv video={video} key={key} />;
+          })}
+          {/* <Assigned /> */}
+        </div>
+      </TrainingTabs>
     </div>
   );
 };
@@ -42,13 +66,25 @@ export function TrainingSUPERV ()  {
   return (
     <>
     <div className={classes.newWrap}>
+      <div style={
+        {display: 'flex', 
+        flexDirection: 'row', 
+        justifyContent: 'right',
+        color: 'white',
+        fontFamily: "Rubik",
+        paddingTop: '30px'}}>
+        <div style={{display: 'flex', flexDirection: 'row', paddingRight: '40px'}}>
+          <p style={{paddingRight: '8px'}}>From: </p>
+          <DatePicker selected={startDate} onChange={(startDate) => setStartDate(startDate)} />
+        </div>
+        <div style={{display: 'flex', flexDirection: 'row', paddingRight: '40px'}}>
+          <p style={{paddingRight: '8px'}}>To: </p>
+          <DatePicker selected={endDate} onChange={(endDate) => setEndDate(endDate)} />
+        </div>
+      </div>
       <div className={classes.search}>
         < FiSearch color="white" size="20px"/>
         <input className={classes.searchBar} placeholder="Search..." onChange={event => setQuery(event.target.value)} />
-          <div classname={classes.picker}>
-          <DatePicker selected={startDate} onChange={(startDate) => setStartDate(startDate)} />
-          <DatePicker selected={endDate} onChange={(endDate) => setEndDate(endDate)} />
-          </div>
       </div>
       <TrainingTabs labels={["General"]}>
         <div className={classes.contentWrap}>
@@ -65,7 +101,7 @@ export function TrainingSUPERV ()  {
               return video;
             } 
           }).map((video, key) => {
-            return <ThumbCard video={video} key={key} />;
+            return <ThumbCardSuperv video={video} key={key} />;
           })}
           {/* <Assigned /> */}
         </div>
