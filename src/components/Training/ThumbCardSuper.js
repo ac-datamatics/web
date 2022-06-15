0d91eac0600d12f2437539539db6a4bc928f0dff
fernandanavamoya <fernava023@gmail.com>
2022-06-14 19:58:53 -0500

    assigned privileges front updated

--- src/components/Training/ThumbCardSuper.js
+++ src/components/Training/ThumbCardSuper.js
@@ -3,26 +3,43 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import { BsThreeDots } from "react-icons/bs";
 
 import classes from "./ThumbCard.module.css";
 import VideoDetails from "./VideoDetails";
 import { useState } from "react";
 
-const ThumbCard = ({ video }) => {
+const ThumbCardSuper = ({ video }) => {
   const [open, setOpen] = useState(false);
+  const [isAssigned, setIsAssigned] = useState(video.is_assigned);
 
   const handleModalOpen = () => {
     setOpen(true);
   };
+  
+  const handleSelectAssigned = (event) => {
+    setIsAssigned(event.target.value);
+    fetch('https://2uxbgsvox5.execute-api.us-east-1.amazonaws.com/Datamatics/video', {
+      method: 'PATCH',
+      body: {
+        "agentUsername": video.agentUsername,
+        "callStartUTCDate": video.callStartUTCDate,
+        "updateKey": "is_assigned",
+        "updateValue": event.target.value
+      }
+    }).then((response) => response.json())
+    .then((_data) => {
+      console.debug(_data)
+    })
+    .catch((err) => console.error(err));
+  }
 
   return (
     <>
       <Card className={classes.root} raised={true}>
         <div style={{backgroundColor: '#232f3e', display: 'flex', justifyContent: 'right', padding: '10px 10px 0px 0px'}}>
-          <select className={classes.customSelect}>
-            <option selected="selected">Not Assigned</option>
-            <option>Assigned</option>
+          <select value={isAssigned} onChange={handleSelectAssigned} className={classes.customSelect}>
+            <option value="false">Not Assigned</option>
+            <option value="true">Assigned</option>
           </select>
         </div>
         <CardActionArea onClick={handleModalOpen}>
@@ -68,4 +85,4 @@ const ThumbCard = ({ video }) => {
   );
 };
 
-export default ThumbCard;
+export default ThumbCardSuper;
